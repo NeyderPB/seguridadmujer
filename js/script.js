5bb8b057bfa49dc7c8576076617862ca8d75e281
Neyder Piedrahita <neiderpiedrahita@hotmail.com>
2018-07-18 20:07:00 -0500

    aja

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*var miVariable=1;
+var miVariable2 =5;
+
+var resultado = miVariable + miVariable2;
+console.log(resultado);*/
+
+/*var miVariable=[1, 2, 3];
+console.log(miVariable);*/
+
+/*var miVariable=1;
+if(miVariable ==0){
+	alert(" La Variable vale 0")
+}else{
+	alert("La Variable no vale 0")
+}*/
+var miVariable=["lunes", "martes", "miecoles", "jueves"]
+for (dia in miVariable) {
+	console.log(miVariable[dia]);
+}
